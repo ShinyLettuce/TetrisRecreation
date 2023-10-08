94c03680545b7b5ac2cf4c1c278e8b03d6164389
ShinyLettuce <je03@live.se>
2023-10-09 01:22:30 +0200

    Removed outdated comment

--- Source/level.cpp
+++ Source/level.cpp
@@ -66,43 +66,6 @@ void Level::piece_movement()
     gravity_counter++;
 }
 
-//void Level::piece_rotation()
-//{
-//    //Move back to tetromino controller
-//
-//    if (hello.rotation_input != 0)
-//    {
-//        //hello.rotation_index++;
-//        for (int y = -1; y < 3; y++)
-//        {
-//            for (int x = -1; x < 3; x++)
-//            {
-//                switch (hello.rotation_index % 4)
-//                {
-//                case(0):
-//                    //90 degrees
-//                    hello.current_grid[(x + 1) + (y + 1) * 4] = hello.shapes.grid_S[(-y + 1) + (x + 1) * 4];
-//                    break;
-//                case(1):
-//                    //180 degrees
-//                    hello.current_grid[(x + 1) + (y + 1) * 4] = hello.shapes.grid_S[(-x + 1) + (-y + 1) * 4];
-//                    break;
-//                case(2):
-//                    //270 degrees
-//                    hello.current_grid[(x + 1) + (y + 1) * 4] = hello.shapes.grid_S[(y + 1) + (-x + 1) * 4];
-//                    break;
-//                case(3):
-//                    //360 degrees
-//                    hello.current_grid[(x + 1) + (y + 1) * 4] = hello.shapes.grid_S[(x + 1) + (y + 1) * 4];
-//                    break;
-//                    
-//                }
-//            }
-//        }
-//        hello.rotation_index++;
-//    }
-//}
-
 void Level::piece_lock()
 {
     for (int i = 0; i < hello.piece_grid_side; i++)
