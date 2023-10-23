a2c398487a060bf977cfa01499b4eb460c686ca5
ShinyLettuce <je03@live.se>
2023-10-23 10:14:14 +0200

    Created circle aabb collision for voronoi region 1

--- Source/colission.cpp
+++ Source/colission.cpp
@@ -1,19 +1,76 @@
 #include "collision.h"
 
+bool Collision_Test::aabb_circle_collision(Vector2 aabb_pos, int aabb_width, int aabb_height, Vector2 circle_pos, int circle_radius)
+{
+	if (circle_pos.x < aabb_pos.x)
+	{
+		if (circle_pos.y > aabb_pos.y && circle_pos.y < aabb_pos.y + aabb_height)
+		{
+			//4
+		}
+		else if (circle_pos.y < aabb_pos.y)
+		{
+			//1
+			if (sqrt((circle_pos.x - aabb_pos.x) * (circle_pos.x - aabb_pos.x) + (circle_pos.y - aabb_pos.y) * (circle_pos.y - aabb_pos.y)) <= circle_radius)
+			{
+				return true;
+			}
+		}
+		else if (circle_pos.y > aabb_pos.y + aabb_height)
+		{
+			//6
+		}
+	}
+	if (circle_pos.x > aabb_pos.x && circle_pos.x < aabb_width)
+	{
+		if (circle_pos.y < aabb_pos.y)
+		{
+			//2
+		}
+		else if (circle_pos.y > aabb_pos.y + aabb_height)
+		{
+			//7
+		}
+	}
+	if (circle_pos.x > aabb_pos.x + aabb_width)
+	{
+		if (circle_pos.y > aabb_pos.y && circle_pos.y < aabb_pos.y + aabb_height)
+		{
+			//5
+		}
+		else if (circle_pos.y < aabb_pos.y)
+		{
+			//3
+		}
+		else if (circle_pos.y > aabb_pos.y + aabb_height)
+		{
+			//8
+		}
+	}
+	return false;
+
+}
+
 void Collision_Test::update()
 {
-	if (sqrt((GetMouseX() - obj1_pos.x) * (GetMouseX() - obj1_pos.x) + (GetMouseY() - obj1_pos.y) * (GetMouseY() - obj1_pos.y)) <= circle_radius && IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+	if (sqrt((GetMouseX() - obj1_pos.x) * (GetMouseX() - obj1_pos.x) + (GetMouseY() - obj1_pos.y) * (GetMouseY() - obj1_pos.y)) <= circle_rad && IsMouseButtonDown(MOUSE_LEFT_BUTTON))
 	{
 		circle_color = RED;
 		obj1_pos.x = (float)GetMouseX();
 		obj1_pos.y =(float)GetMouseY();
 	}
+
+	if (aabb_circle_collision(obj2_pos, aabbox_width, aabbox_height, obj1_pos, circle_rad))
+	{
+		circle_color = GREEN;
+	}
+
 }
 
 void Collision_Test::render()
 {
-	DrawCircle((int)obj1_pos.x, (int)obj1_pos.y, (float)circle_radius, circle_color);
-	DrawRectangle((int)obj2_pos.x, (int)obj2_pos.y, aabb_size, aabb_size, WHITE);
+	DrawCircle((int)obj1_pos.x, (int)obj1_pos.y, (float)circle_rad, circle_color);
+	DrawRectangle((int)obj2_pos.x, (int)obj2_pos.y, aabbox_width, aabbox_height, WHITE);
 
 	DrawText("Collision screen", 100, 100, 60, WHITE);
 }
--- Source/collision.h
+++ Source/collision.h
@@ -6,9 +6,12 @@ struct Collision_Test
 {
 	Vector2 obj1_pos = {200,300};
 	Vector2 obj2_pos = { 500,200 };
-	int circle_radius = 100;
-	int aabb_size = 200;
+	int circle_rad = 100;
+	int aabbox_height = 200;
+	int aabbox_width = 200;
 	Color circle_color = WHITE;
+
+	bool aabb_circle_collision(Vector2 aabb_pos, int aabb_width, int aabb_height, Vector2 circle_pos, int circle_radius);
 
 	void update();
 	void render();
