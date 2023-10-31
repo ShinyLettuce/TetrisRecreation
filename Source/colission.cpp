0823eb2bf420c95409f1fe76ecf85fe0592faef9
ShinyLettuce <je03@live.se>
2023-10-31 11:40:23 +0100

    Changed render functions to take vectors and changed aabb width and height to a vector, added line start and end position

--- Source/colission.cpp
+++ Source/colission.cpp
@@ -9,11 +9,11 @@ bool Collision_Test::point_circle_collision(int point_x, int point_y, Vector2 ci
 	return false;
 }
 
-bool Collision_Test::point_aabb_collision(int point_x, int point_y, Vector2 aabb_pos, int aabb_width, int aabb_height)
+bool Collision_Test::point_aabb_collision(int point_x, int point_y, Vector2 aabb_pos, Vector2 aabb_w_h)
 {
-	if (point_x <= aabb_pos.x + aabb_width &&
+	if (point_x <= aabb_pos.x + aabb_w_h.x &&
 		point_x >= aabb_pos.x &&
-		point_y <= aabb_pos.y + aabb_height &&
+		point_y <= aabb_pos.y + aabb_w_h.y &&
 		point_y >= aabb_pos.y)
 	{
 		return true;
@@ -21,11 +21,11 @@ bool Collision_Test::point_aabb_collision(int point_x, int point_y, Vector2 aabb
 	return false;
 }
 
-void Collision_Test::clamp_aabb_to_mouse(Vector2& obj_position, int aabb_width, int aabb_height, bool& selection)
+void Collision_Test::clamp_aabb_to_mouse(Vector2& obj_position, Vector2 aabb_w_h, bool& selection)
 {
 	selection = true;
-	obj_position.x = (float)GetMouseX() - aabb_width / 2;
-	obj_position.y = (float)GetMouseY() - aabb_height / 2;
+	obj_position.x = (float)GetMouseX() - aabb_w_h.x / 2;
+	obj_position.y = (float)GetMouseY() - aabb_w_h.y / 2;
 }
 
 void Collision_Test::clamp_circle_to_mouse(Vector2& obj_position, bool& selection)
@@ -35,7 +35,7 @@ void Collision_Test::clamp_circle_to_mouse(Vector2& obj_position, bool& selectio
 	obj_position.y = (float)GetMouseY();
 }
 
-bool Collision_Test::aabb_circle_collision(Vector2 aabb_pos, int aabb_width, int aabb_height, Vector2 circle_pos, int circle_radius) // TRIED AND TRUE
+bool Collision_Test::aabb_circle_collision(Vector2 aabb_pos, Vector2 aabb_w_h, Vector2 circle_pos, int circle_radius) // TRIED AND TRUE
 {
 	Vector2 closest_pos = { 0,0 };
 	
@@ -43,9 +43,9 @@ bool Collision_Test::aabb_circle_collision(Vector2 aabb_pos, int aabb_width, int
 	{
 		closest_pos.x = aabb_pos.x;
 	}
-	else if (circle_pos.x > aabb_pos.x + aabb_width)
+	else if (circle_pos.x > aabb_pos.x + aabb_w_h.x)
 	{
-		closest_pos.x = aabb_pos.x + aabb_width;
+		closest_pos.x = aabb_pos.x + aabb_w_h.x;
 	}
 	else
 	{
@@ -55,9 +55,9 @@ bool Collision_Test::aabb_circle_collision(Vector2 aabb_pos, int aabb_width, int
 	{
 		closest_pos.y = aabb_pos.y;
 	}
-	else if (circle_pos.y > aabb_pos.y + aabb_height)
+	else if (circle_pos.y > aabb_pos.y + aabb_w_h.y)
 	{
-		closest_pos.y = aabb_pos.y + aabb_height;
+		closest_pos.y = aabb_pos.y + aabb_w_h.y;
 	}
 	else
 	{
@@ -79,12 +79,12 @@ bool Collision_Test::circle_circle_collision(Vector2 circle1_pos, int circle1_ra
 	return false;
 }
 
-bool Collision_Test::aabb_aabb_collision(Vector2 aabb1_pos, int aabb1_width, int aabb1_height, Vector2 aabb2_pos, int aabb2_width, int aabb2_height)
+bool Collision_Test::aabb_aabb_collision(Vector2 aabb1_pos, Vector2 aabb1_w_h, Vector2 aabb2_pos, Vector2 aabb2_w_h)
 {
-	if (aabb1_pos.x <= aabb2_pos.x + aabb2_width &&
-		aabb1_pos.x + aabb1_width >= aabb2_pos.x &&
-		aabb1_pos.y <= aabb2_pos.y + aabb2_height &&
-		aabb1_pos.y + aabb1_height >= aabb2_pos.y)
+	if (aabb1_pos.x <= aabb2_pos.x + aabb2_w_h.x &&
+		aabb1_pos.x + aabb1_w_h.x >= aabb2_pos.x &&
+		aabb1_pos.y <= aabb2_pos.y + aabb2_w_h.y &&
+		aabb1_pos.y + aabb1_w_h.y >= aabb2_pos.y)
 	{
 		return true;
 	}
@@ -132,11 +132,11 @@ void Collision_Test::update()
 		{
 			clamp_circle_to_mouse(obj1_pos, obj1_selected);
 		}
-		if (point_aabb_collision(GetMouseX(), GetMouseY(), obj2_pos, aabbox_width, aabbox_height) && IsMouseButtonDown(MOUSE_LEFT_BUTTON) && !obj1_selected)
+		if (point_aabb_collision(GetMouseX(), GetMouseY(), obj2_pos, aabbox_width_height) && IsMouseButtonDown(MOUSE_LEFT_BUTTON) && !obj1_selected)
 		{
-			clamp_aabb_to_mouse(obj2_pos, aabbox_width, aabbox_height, obj2_selected);
+			clamp_aabb_to_mouse(obj2_pos, aabbox_width_height, obj2_selected);
 		}
-		if (aabb_circle_collision(obj2_pos, aabbox_width, aabbox_height, obj1_pos, circle_rad))
+		if (aabb_circle_collision(obj2_pos, aabbox_width_height, obj1_pos, circle_rad))
 		{
 			obj1_color = GREEN;
 			obj2_color = GREEN;
@@ -160,15 +160,15 @@ void Collision_Test::update()
 	case(Collision_Type::CIRCLE_V_LINE):
 		break;
 	case(Collision_Type::AABB_V_AABB):
-		if (point_aabb_collision(GetMouseX(), GetMouseY(), obj1_pos, aabbox_width, aabbox_height) && IsMouseButtonDown(MOUSE_LEFT_BUTTON) && !obj2_selected)
+		if (point_aabb_collision(GetMouseX(), GetMouseY(), obj1_pos, aabbox_width_height) && IsMouseButtonDown(MOUSE_LEFT_BUTTON) && !obj2_selected)
 		{
-			clamp_aabb_to_mouse(obj1_pos, aabbox_width, aabbox_height, obj1_selected);
+			clamp_aabb_to_mouse(obj1_pos, aabbox_width_height, obj1_selected);
 		}
-		if (point_aabb_collision(GetMouseX(), GetMouseY(), obj2_pos, aabbox_width, aabbox_height) && IsMouseButtonDown(MOUSE_LEFT_BUTTON) && !obj1_selected)
+		if (point_aabb_collision(GetMouseX(), GetMouseY(), obj2_pos, aabbox_width_height) && IsMouseButtonDown(MOUSE_LEFT_BUTTON) && !obj1_selected)
 		{
-			clamp_aabb_to_mouse(obj2_pos, aabbox_width, aabbox_height, obj2_selected);
+			clamp_aabb_to_mouse(obj2_pos, aabbox_width_height, obj2_selected);
 		}
-		if (aabb_aabb_collision(obj2_pos, aabbox_width, aabbox_height, obj1_pos, aabbox_width, aabbox_height))
+		if (aabb_aabb_collision(obj2_pos, aabbox_width_height, obj1_pos, aabbox_width_height))
 		{
 			obj1_color = GREEN;
 			obj2_color = GREEN;
@@ -182,18 +182,18 @@ void Collision_Test::render()
 	switch (collision_type)
 	{
 	case(Collision_Type::CIRCLE_V_AABB):
-		DrawCircle((int)obj1_pos.x, (int)obj1_pos.y, (float)circle_rad, obj1_color);
-		DrawRectangle((int)obj2_pos.x, (int)obj2_pos.y, aabbox_width, aabbox_height, obj2_color);
+		DrawCircleV(obj1_pos, (float)circle_rad, obj1_color);
+		DrawRectangleV(obj2_pos, aabbox_width_height, obj2_color);
 		break;
 	case(Collision_Type::CIRCLE_V_CIRCLE):
-		DrawCircle((int)obj1_pos.x, (int)obj1_pos.y, (float)circle_rad, obj1_color);
-		DrawCircle((int)obj2_pos.x, (int)obj2_pos.y, (float)circle_rad, obj2_color);
+		DrawCircleV(obj1_pos, (float)circle_rad, obj1_color);
+		DrawCircleV(obj2_pos, (float)circle_rad, obj2_color);
 		break;
 	case(Collision_Type::CIRCLE_V_LINE):
 		break;
 	case(Collision_Type::AABB_V_AABB):
-		DrawRectangle((int)obj1_pos.x, (int)obj1_pos.y, aabbox_width, aabbox_height, obj1_color);
-		DrawRectangle((int)obj2_pos.x, (int)obj2_pos.y, aabbox_width, aabbox_height, obj2_color);
+		DrawRectangleV(obj1_pos, aabbox_width_height, obj1_color);
+		DrawRectangleV(obj2_pos, aabbox_width_height, obj2_color);
 		break;
 	}
 
--- Source/collision.h
+++ Source/collision.h
@@ -17,9 +17,10 @@ struct Collision_Test
 
 	Vector2 obj1_pos = {200,300};
 	Vector2 obj2_pos = { 500,200 };
+	Vector2 line_pos1 = {};
+	Vector2 line_pos2 = {};
+	Vector2 aabbox_width_height = {200,200};
 	int circle_rad = 100;
-	int aabbox_height = 200;
-	int aabbox_width = 200;
 	Color obj1_color = WHITE;
 	Color obj2_color = WHITE;
 
@@ -27,13 +28,13 @@ struct Collision_Test
 	bool obj2_selected = false;
 
 	bool point_circle_collision(int point_x, int point_y, Vector2 circle1_pos, int circle1_radius);
-	bool point_aabb_collision(int point_x, int point_y, Vector2 aabb_pos, int aabb_width, int aabb_height);
-	void clamp_aabb_to_mouse(Vector2& obj_position, int aabb_width, int aabb_height, bool& selection);
+	bool point_aabb_collision(int point_x, int point_y, Vector2 aabb_pos, Vector2 aabb_w_h);
+	void clamp_aabb_to_mouse(Vector2& obj_position, Vector2 aabb_w_h, bool& selection);
 	void clamp_circle_to_mouse(Vector2& obj_position, bool& selection);
 
-	bool aabb_circle_collision(Vector2 aabb_pos, int aabb_width, int aabb_height, Vector2 circle_pos, int circle_radius);
+	bool aabb_circle_collision(Vector2 aabb_pos, Vector2 aabb_w_h, Vector2 circle_pos, int circle_radius);
 	bool circle_circle_collision(Vector2 circle1_pos, int circle1_radius, Vector2 circle2_pos, int circle2_radius);
-	bool aabb_aabb_collision(Vector2 aabb1_pos, int aabb1_width, int aabb1_height, Vector2 aabb2_pos, int aabb2_width, int aabb2_height);
+	bool aabb_aabb_collision(Vector2 aabb1_pos, Vector2 aabb1_w_h, Vector2 aabb2_pos, Vector2 aabb2_w_h);
 
 	void update();
 	void render();
