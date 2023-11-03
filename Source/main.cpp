65b4025c7ccf9f38a2482bfe2af4c0eac3a0922f
ShinyLettuce <je03@live.se>
2023-11-03 15:00:09 +0100

    binary file reading works

--- Source/main.cpp
+++ Source/main.cpp
@@ -21,14 +21,34 @@ int main(void)
 
     Game_State game;
 
-    std::fstream score_list(".\\Assets\\text\\highscore.txt");
-
+    //std::fstream score_list(".\\Assets\\text\\highscore.txt");
     High_Score_Entry h;
-    while (score_list >> h.name >> h.score)
+    //while (score_list >> h.name >> h.score)
+    //{
+    //    game.high_score.list.push_back(h);
+    //}
+    //score_list.close();
+
+    FILE* fp_input;
+    errno_t errt;
+    //score_list.open(".\\Assets\\text\\highscore.txt");
+    errt = fopen_s(&fp_input, ".\\Assets\\text\\highscore.bin", "rb");
+    if (errt != 0)
     {
-        game.high_score.list.push_back(h);
+        //nothing
     }
-    score_list.close();
+    else
+    {
+        
+        while (!feof(fp_input))
+        {
+            fread(&h.name, sizeof(h.name), 1, fp_input);
+            fread(&h.score, sizeof(h.score), 1, fp_input);
+            game.high_score.list.push_back(h);
+        }
+        
+    }
+    fclose(fp_input);
 
     game.game_state.push(Game_State::GAME_STATE::MAIN_MENU);
 
@@ -46,14 +66,25 @@ int main(void)
 
     CloseAudioDevice();
     Media::media_unload();
-    
-    score_list.open(".\\Assets\\text\\highscore.txt");
-    for(High_Score_Entry saved : game.high_score.list)
+    FILE* fp_output;
+    errno_t err;
+    //score_list.open(".\\Assets\\text\\highscore.txt");
+    err = fopen_s(&fp_output,".\\Assets\\text\\highscore.bin", "wb");
+    if (err != 0)
     {
-        score_list << saved.name;
-        score_list << saved.score;
+        //nothing
     }
-    score_list.close();
+    else
+    {
+        for(High_Score_Entry saved : game.high_score.list)
+        {
+             //score_list << saved.name;
+             //score_list << saved.score;
+
+            fwrite(&saved, sizeof(saved), 1, fp_output);
+        }
+    }
+    //score_list.close();
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
